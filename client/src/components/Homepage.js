b5f21ecaf432e3cfa677d42495c1f8a8b86e1ddd
Kara P <quetzlcoatlus@gmail.com>
2021-03-16 12:09:07 +0100

    basic homepage component

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Homepage extends React.Component {
+  render() {
+    return (
+      <div>
+        ITS THE HOMEPAGE
+      </div>
+    )
+  }
+}
